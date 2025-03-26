dcb21534160dc783253b60e2df4d806034ff408e
k <iliakripa@mail.ru>
2025-03-27 01:00:23 +0300

    fix error message

--- resources/web/errors.go
+++ resources/web/errors.go
@@ -3,7 +3,6 @@ package web
 type errorDetail string
 
 const (
-	UnauthorizedError    errorDetail = "authorisation error"
 	InternalServerError  errorDetail = "server error"
 	TokenExpectedError   errorDetail = "token expected"
 	TokenInvalidError    errorDetail = "invalid token"
